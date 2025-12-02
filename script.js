090b3b5fc87502585c39c0a18b97f64780f700b1
Sourour2B <sourour13@hotmail.fr>
2025-12-02 21:54:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // ATTENTION : Le script utilise l'heure locale de l'utilisateur.
 
 
-const DECEMBER= 11; // JavaScript compte les mois de 0 (Jan) à 11 (Dec)
+const DECEMBER= 10; // JavaScript compte les mois de 0 (Jan) à 11 (Dec)
 const CURRENT_YEAR = new Date().getFullYear(); // Année actuelle (ex: 2025)
 
 
@@ -203,6 +203,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
 });
+
 
 
 
