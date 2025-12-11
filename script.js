1816e1886f9f57d396a8b4063c9e54b14c81c154
Sourour2B <sourour13@hotmail.fr>
2025-12-11 22:34:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -135,16 +135,7 @@ const ADVENT_CONTENT = {
 
 
 document.addEventListener('DOMContentLoaded', () => {
-const today = new Date();
-    const currentDay = today.getDate();
-    const currentMonth = today.getMonth();
-    const currentYear = today.getFullYear();
-    
-        
-    // Déterminer si nous sommes en Décembre de l'année configurée
-    const isDecember = (currentMonth === DECEMBER&& currentYear === CURRENT_YEAR);
-
-   // --- MODE TEST ACTIF : Simule le 24 décembre pour déverrouiller toutes les cases ---
+// --- MODE TEST ACTIF : Simule le 24 décembre pour déverrouiller toutes les cases ---
     const simulatedDate = new Date(CURRENT_YEAR, DECEMBER, 24); 
     
     const today = simulatedDate; // On utilise la date simulée
@@ -152,6 +143,13 @@ const today = new Date();
     const currentMonth = today.getMonth(); // Sera 11 (Décembre)
     const currentYear = today.getFullYear(); // Sera l'année configurée
     // --- MODE TEST TERMINÉ ---
+    const currentYear = today.getFullYear();
+    
+        
+    // Déterminer si nous sommes en Décembre de l'année configurée
+    const isDecember = (currentMonth === DECEMBER&& currentYear === CURRENT_YEAR);
+
+  
 
     // 1. Gérer le déblocage des cases au chargement
     days.forEach(day => {
@@ -210,6 +208,7 @@ const today = new Date();
     });
 
 });
+
 
 
 
