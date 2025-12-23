843f7274fe0662a752abe40935c55aa6826d6ee4
Sourour2B <sourour13@hotmail.fr>
2025-12-24 00:58:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -128,7 +128,7 @@ const ADVENT_CONTENT = {
     },
     24: { 
         title: "Jour 24 : Joyeux Réveillon !", 
-        text: "Joyeux Réveillon ! Voici un cadeau spécial : 🎁 <a href="https://youtu.be/I0dzWSb1hgo target="_blank"">Message du Père Noël</a>",
+        text: "Joyeux Réveillon ! Voici un cadeau spécial : 🎁<br><br><iframe width="560" height="315" src="https://www.youtube.com/watch?v=I0dzWSb1hgo" frameborder="0" allowfullscreen></iframe>",
         image: "assets/contenu-1.jpg" 
     },
 };
@@ -209,6 +209,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
 });
+
 
 
 
